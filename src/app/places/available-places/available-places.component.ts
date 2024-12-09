e4c7d101f687fd3d4cc90be1fc865ba646c97b35
csmith120 <taskmaster3654@gmail.com>
2024-12-09 13:37:12 -0600

    added a complete function vid 9

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -14,15 +15,22 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 })
 export class AvailablePlacesComponent implements OnInit{
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false)
   private httpClient = inject(HttpClient);
   private destoryRef = inject(DestroyRef);
 
   // constructors(private httpClient: HttpClient) {}
 
   ngOnInit() {
-    const subscription = this.httpClient.get<{places: Place[] }>('http://localhost:3000/places').subscribe({
-      next: (resData) => {
-        console.log(resData.places);
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{places: Place[] }>('http://localhost:3000/places').pipe(
+      map((resData) => resData.places)
+    ).subscribe({
+      next: (places) => {
+        this.places.set(places);
+      },
+      complete: () => {
+        this.isFetching.set(false);
       }
     });
 
